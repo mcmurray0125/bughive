376ee5577a5113e9b22f15898d3c3db6432d15b9
Michael Murray <mcmurray0125@gmail.com>
2023-05-13 22:29:22 -0500

    created useForms hook

---
+++ client/src/utilities/formValidation/useForms.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+
+const useForm = (callback, initialValues, validate) => {
+  const [values, setValues] = useState(initialValues);
+
+  //new state for errors
+  const [errors, setErrors] = useState({});
+
+  const [isSubmitting, setIsSubmitting] = useState(false); //isolates the submit callback function to only run if there is an error change.
+
+
+  const handleChange = (event) => {
+      let value;
+
+      if (event.target.type === "checkbox") {
+        value = event.target.checked;
+      } else if (
+        event.target.type === "select" ||
+        event.target.type === "select-multiple"
+      ) {
+        value = Array.from(
+          event.target.selectedOptions,
+          (option) => option.value
+        );
+      } else {
+        value = event.target.value;
+      }
+
+      const name = event.target.name;
+
+      setValues({
+        ...values,
+        [name]: value,
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    validate(values).then((res) => {
+      setIsSubmitting(true);
+      setErrors(res); //handle validation
+    });
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      callback();
+    }
+  }, [errors]);
+
+  return {
+    handleChange,
+    handleSubmit,
+    values,
+    errors,
+  };
+};
+
+export default useForm;

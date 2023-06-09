e418692205aebbb6e8d6d4429f3fb069151b695c
Michael Murray <mcmurray0125@gmail.com>
2023-06-09 17:12:49 -0500

    clicking chart navigates to tickets page.

--- client/src/views/Index.jsx
+++ client/src/views/Index.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Container } from "reactstrap"
 import ProjectTable from "../components/Tables/ProjectTable";
 import TicketsChart from "../components/Charts/TicketsChart";
 import API from "../utilities/API.js"
+import { useAuth } from "../contexts/AuthContext"
 
 export default function Index() {
     const [ticketsData, setTicketsData] = useState([])
+    const { rootPath } = useAuth()
+    const navigate = useNavigate()
 
     useEffect(() => {
         const abortController = new AbortController();
@@ -29,19 +33,24 @@ export default function Index() {
           abortController.abort();
         };
       }, []);
-    
+      
+      function handleChartClick(e) {
+        e.preventDefault()
+        navigate(`${rootPath}/tickets`)
+      }
+
     return(
         <Container className="dashboard-container py-2" fluid>
             <ProjectTable/>
             <div className="charts-container my-3">
-              <div className="chart-wrapper card">
-                <TicketsChart ticketsData={ticketsData} focus="status" /> {/* Chart for status */}
+              <div className="chart-wrapper card" onClick={handleChartClick}>
+                <TicketsChart ticketsData={ticketsData} focus="status" /> 
               </div>
-              <div className="chart-wrapper card">
-                <TicketsChart ticketsData={ticketsData} focus="priority" /> {/* Chart for priority */}
+              <div className="chart-wrapper card" onClick={handleChartClick}>
+                <TicketsChart ticketsData={ticketsData} focus="priority" />
               </div>
-              <div className="chart-wrapper card">
-                <TicketsChart ticketsData={ticketsData} focus="type" /> {/* Chart for type */}
+              <div className="chart-wrapper card" onClick={handleChartClick}>
+                <TicketsChart ticketsData={ticketsData} focus="type" />
               </div>
             </div>
         </Container>
